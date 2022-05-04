530617fccead9b2b75c4b6f43af76940ecc4a3b5
Jeffrey Tilson <jtilson@renci.org>
2022-05-04 12:28:04 -0400

    Restructured and cleaned up the logger code.

--- utilities/utilities.py
+++ utilities/utilities.py
@@ -6,6 +6,8 @@
 # Add timing calls
 # That can be used by any of the ADCIRC support tools
 #
+# Rebuilt logging mewthod baseed in part on the work of P. Owen's Supervisor code
+#
 # RENCI 2020
 #############################################################
 
@@ -16,48 +18,47 @@ import json
 
 LOGGER = None
 
-class Utilities:
+class utilities:
     """
+    Class to manage the logging setup.
 
     """
-    def __init__(self, instanceid=None):
+    @staticmethod
+    def init_logging(subdir=None, config_file=None):
         """
         Initialize the Utilities class, set up logging
         """
         global LOGGER
-        self.config = self.load_config()
-
-        if LOGGER is None and self.config["DEFAULT"]["LOGGING"]:
-            log = self.initialize_logging(instanceid)
+        if LOGGER is None:
+            config_data = utilities.load_config(yaml_file=config_file)
+            log = utilities.initialize_logging(subdir=subdir, config=config_data)
             LOGGER = log
-        self.log = LOGGER
+        utilities.log = LOGGER
 
-#############################################################
-# Logging
-
-    def initialize_logging(self, instanceid=None):
+    def initialize_logging(subdir=None, config=None):
         """
-        Initialize project logging
-        instanceid is a subdirectory to be created under LOG_PATH
+        Log file get saved to $LOG_PATH/subdir. LOG_PATH defaults to '.'
+
+        Parameters
+            config: dictionary containing logging settings (usually this is main.yml)
+            subdir: A subdirectory constructed beneath the value in LOG_PATH 
+        Returns
+            logger handle
         """
         # logger = logging.getLogger(__name__)
-        logger = logging.getLogger("adda_services") # We could simply add the instanceid here as well
-        log_level = self.config["DEFAULT"].get('LOGLEVEL', 'DEBUG')
-        # log_level = getattr(logging, self.config["DEFAULT"].get('LOGLEVEL', 'DEBUG'))
+        logger = logging.getLogger("ast_services") # We could simply add the instanceid here as well
+
+        log_level = config["DEFAULT"].get('LOGLEVEL', 'DEBUG')
         logger.setLevel(log_level)
 
-        # LogFile = self.config['LOG_FILE']
-        # LogFile = '{}.{}.log'.format(thisDomain, currentdatecycle.cdc)
-        #LogFile = 'log'
-        #LogFile = os.getenv('LOG_PATH', os.path.join(os.path.dirname(__file__), 'logs'))
-        if instanceid is not None:
-            Logdir = '/'.join([os.getenv('LOG_PATH','.'),instanceid])
+        if subdir is not None:
+            Logdir = '/'.join([os.getenv('LOG_PATH','.'),subdir])
         else:
-            Logdir = os.getenv('LOG_PATH','.') 
+            Logdir = os.getenv('LOG_PATH','.')
+
         #LogName =os.getenv('LOG_NAME','logs')
         LogName='AdcircSupportTools.log'
         LogFile='/'.join([Logdir,LogName])
-        self.LogFile = LogFile
 
         formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(funcName)s : %(module)s : %(name)s : %(message)s ')
         dirname = os.path.dirname(LogFile)
@@ -67,10 +68,8 @@ class Utilities:
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
         return logger
-
-#############################################################
 # YAML
-    def load_config(self, yaml_file=os.path.join(os.path.dirname(__file__), '../config', 'main.yml')):
+    def load_config(yaml_file=None):
         if yaml_file is None:
             utilities.log.error('Called load_config but didnt specify yaml name: ABORT')
             sys.exit(1)
@@ -81,14 +80,4 @@ class Utilities:
             print('Opened yaml file {}'.format(yaml_file,))
         return config
 
-    def read_config_yml(self, yamlfilename):
-        if not os.path.exists(yamlfilename):
-            raise IOError("Failed to find config file %s" % yamlfilename)
-        # config_file = EnvYAML(yamlfilename)
-        # print(config_file['ADDAHOME'])
-        with open(yamlfilename, 'r') as stream:
-            config_file = yaml.safe_load(stream)
-        return config_file
-
-utilities = Utilities()
 
