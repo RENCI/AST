2ce7b9c8d447721ee670bab8be5dd8e271e68767
Jeffrey Tilson <jtilson@renci.org>
2023-07-20 08:33:40 -0400

    Fixed erroneous error msg

--- harvester/fetch_adcirc_data.py
+++ harvester/fetch_adcirc_data.py
@@ -61,7 +61,7 @@ def get_adcirc_stations_fort61_style(fname=None):
 
     """
     if fname is None:
-        utilities.log.error('No Fort63_style ADCIRC station file assigned: Abort with status 0')
+        utilities.log.error('No Fort61_style ADCIRC station file assigned: Abort with status 0')
         sys.exit(0)
     df = pd.read_csv(fname, index_col=0, header=0, skiprows=[1],dtype=str)
     adcirc_stations_list = df["stationid"].to_list()
